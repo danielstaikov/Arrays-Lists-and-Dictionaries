9276c5db8a88fbdfe2297f40ba28d61db8cc1d7f
Dani0000 <daniel.staikov@gmail.com>
2017-06-16 17:33:03 +0300

    Integer to Base.cs and 2.0 upload

--- Integer to Base 2.0/Integer to Base 2.0.cs
+++ Integer to Base 2.0/Integer to Base 2.0.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Integer_to_Base
+namespace Integer_to_Base_2._0
 {
     class Program
     {
@@ -14,17 +14,19 @@ namespace Integer_to_Base
             int toBase = int.Parse(Console.ReadLine());
             Console.WriteLine(IntegerToBase(number, toBase));
         }
-        static string IntegerToBase(long number, int toBase)
+        public static string IntegerToBase(long number, int toBase)
         {
+            string remainder = string.Empty;
             string result = string.Empty;
+
             while (number > 0)
             {
-                long rem = number % toBase;
-                result = $"{rem}" + result;
-                number = number / toBase;
+                remainder = (number % toBase).ToString();
+                result = result.Insert(0, remainder);
+                number /= toBase;
             }
+
             return result;
         }
-
     }
 }
